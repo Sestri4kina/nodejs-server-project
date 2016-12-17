419abd1c9b54e366f15a9497a249dbec40526ede
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-17 23:53:39 +0200

    Add project files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Sestri4kina on 17.12.2016.
+ */
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url );
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey guys! Nice to see you!');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("ok, now we're listening to port 3000");
