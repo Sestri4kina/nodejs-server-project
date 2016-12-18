4b18c192ae7baf997c697a982a584c7f91c99711
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-18 18:48:33 +0200

    Take a readable stream and pipe it to response

--- js/app.js
+++ js/app.js
@@ -9,16 +9,11 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readable.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writable.txt');
-
-//add .pipe method to readable stream, this does the same as previous .on
-myReadStream.pipe(myWriteStream);
-
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url );
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey guys! Nice to see you!');
+    var myReadStream = fs.createReadStream(__dirname + '/readable.txt', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
