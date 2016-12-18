35f7c71f17800b2bf1ea390df47b095068f7bdd6
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-18 18:09:37 +0200

    Add writable stream

--- js/app.js
+++ js/app.js
@@ -10,10 +10,12 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readable.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writable.txt');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received: ');
-    console.log(chunk);
+    //console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
 /*
