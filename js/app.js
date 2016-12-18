608ecff08260c3e922a93a1b6c025aa30dc44ae5
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-18 19:22:12 +0200

    Serve JSON data to the browser

--- js/app.js
+++ js/app.js
@@ -11,9 +11,18 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url );
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Nastia',
+        job: 'JS coder',
+        age: 29
+    };
+    res.end(JSON.stringify(myObj));
+    /*
     res.writeHead(200, {'Content-Type': 'text/html'});
     var myReadStream = fs.createReadStream('D:/projects/nodejs-server-project/index.html', 'utf8');
     myReadStream.pipe(res);
+    */
 });
 
 server.listen(3000, '127.0.0.1');
